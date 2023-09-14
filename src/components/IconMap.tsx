65dba32fd62853a1a929c4d7fb7f0a148ed3f2c3
DreamDrunker163 <3048710651@qq.com>
2023-09-14 20:43:31 +0800

    feat: 添加了几个图标

--- src/components/IconMap.tsx
+++ src/components/IconMap.tsx
@@ -3,14 +3,30 @@ import {
 	UserOutlined,
 	LockOutlined,
 	MobileOutlined,
-	CodeSandboxOutlined
-} from "@ant-design/icons"
+	CodeSandboxOutlined,
+	ApartmentOutlined,
+	AimOutlined,
+	InsertRowAboveOutlined,
+	SmileOutlined,
+	AccountBookOutlined,
+	DashboardOutlined,
+	LineChartOutlined,
+	TeamOutlined
+} from '@ant-design/icons'
 
 export default {
 	arrRowRight: <ArrowRightOutlined />,
 	userIcon: <UserOutlined />,
 	lockIcon: <LockOutlined />,
 	mobileIcon: <MobileOutlined />,
-	codeIcon: <CodeSandboxOutlined />
+	codeIcon: <CodeSandboxOutlined />,
 
+	department: <ApartmentOutlined />,
+	level: <AimOutlined />,
+	assessment: <InsertRowAboveOutlined />,
+	reward: <SmileOutlined />,
+	salary: <AccountBookOutlined />,
+	dashboard: <DashboardOutlined />,
+	attendance: <LineChartOutlined />,
+	team: <TeamOutlined />
 }
