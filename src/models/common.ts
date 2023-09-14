2daf0acae912630dc33e6eeb8ed0f073903e7de9
DreamDrunker163 <3048710651@qq.com>
2023-09-14 15:55:21 +0800

    feat: 添加了简单的路由守卫，输入账号admin密码123456后跳转到dashboard页面， 如果直接在url修改会强制回到/users/login页面

---
+++ src/models/common.ts
@@ -0,0 +1,44 @@
+export default {
+	namespace: 'common',
+	state: {},
+	subscriptions: {
+		setup({ dispatch, history }: { dispatch: any; history: any }) {
+			// 初始化查询用户是否登陆，app.start阶段执行
+			dispatch({ type: 'queryUserLogin', payload: { history } })
+		}
+	},
+	effects: {
+		*queryUserLogin(
+			{ payload }: { payload: any },
+			{ put, call }: { put: any; call: any }
+		) {
+			const {
+				history,
+				history: {
+					location: { pathname }
+				}
+			} = payload
+			if (
+				pathname !== '/users/login' &&
+				pathname !== '/users/forgetPassword'
+			) {
+				if (
+					!sessionStorage.getItem('userProfile') ||
+					!sessionStorage.getItem('token') ||
+					!sessionStorage.getItem('routeList')
+				) {
+					console.log('路由守卫')
+					history.replace('/users/login')
+				} else {
+					// 用户满足条件，进行登录信息的检测
+					/* 
+
+					*/
+				}
+			} else {
+				// 不需要进行拦截
+				sessionStorage.clear()
+			}
+		}
+	}
+}
--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,28 +1,104 @@
 import React from 'react'
-import { useState } from "react"
-import AccountLogin from "./component/AccountLogin"
+import { useState } from 'react'
+import AccountLogin from './component/AccountLogin'
 import SmCodeLogin from './component/SmCodeLogin'
 // 使用antd的form表单相关组件
 import { Form, Input, Button, Row, Col } from 'antd'
 // 使用antd的图标库
 import IconMap from '@/components/IconMap'
 // 导入图标
-import logoImg from "@/common/img/logo.png"
+import logoImg from '@/common/img/logo.png'
 
-import "./css/login.less"
+import './css/login.less'
 
-const login = () => {
+const login = ({ history }: { history: any }) => {
 	const [form] = Form.useForm()
 	const FormItem = Form.Item
 	const [type, setType] = useState(1)
 
+	interface userInfo {
+		username?: string
+		password?: string
+		mobileNumber?: string
+		code?: string
+	}
+
 	// 当表单完成输入后的提交事件
-	const submitUserInfo = (data: string) => {
-		console.log(data)
+	const submitUserInfo = ({ username, password }: userInfo) => {
+		if (username === 'admin' && password === '123456') {
+			sessionStorage.setItem('token', 'jfalohfas')
+
+			sessionStorage.setItem(
+				'routeList',
+				JSON.stringify([
+					{
+						_id: '1',
+						icon: 'dashboard',
+						zhName: '员工分析',
+						route: '/dashboard'
+					},
+					{
+						_id: '2',
+						icon: 'attendance',
+						zhName: '出勤统计',
+						route: '/attendance'
+					},
+					{ _id: '3', icon: 'team', zhName: '员工', route: '/staff' },
+					{
+						_id: '4',
+						icon: 'department',
+						zhName: '部门',
+						route: '/department'
+					},
+					{
+						_id: '5',
+						icon: 'level',
+						zhName: '职级',
+						route: '/level'
+					},
+					{
+						_id: '6',
+						icon: 'assessment',
+						zhName: '绩效考核',
+						route: '/assessment'
+					},
+					{
+						_id: '7',
+						icon: 'salary',
+						zhName: '调薪记录',
+						route: '/salary'
+					},
+					{
+						_id: '8',
+						icon: 'rewardAndPunishment',
+						zhName: '奖惩记录',
+						route: '/rewardAndPunishment'
+					},
+					{
+						_id: '9',
+						icon: 'bar-chart',
+						zhName: '考勤信息',
+						route: '/attentanceInfo'
+					}
+				])
+			)
+			sessionStorage.setItem(
+				'userProfile',
+				JSON.stringify({
+					accountName: 'admin',
+					department: 'department',
+					identify: 1,
+					userName: '管理员',
+					_id: '0'
+				})
+			)
+			history.push(JSON.parse(sessionStorage['routeList'])[0].route)
+		}
 	}
 
 	// 组件选择的容器函数
-	const ComponentSelector = (props: any) => type ? <AccountLogin {...props} /> : <SmCodeLogin {...props} />
+	const ComponentSelector = (props: any) =>
+		type ? <AccountLogin {...props} /> : <SmCodeLogin {...props} />
 
 	return (
 		<div className="form">
@@ -34,11 +110,29 @@ const login = () => {
 				{/* 选择当前展示的组件 */}
 				{ComponentSelector({ form, FormItem, Input })}
 				<Row>
-					<Button block={true} type="primary" className="loginBtn">登陆</Button>
+					<Button
+						block={true}
+						type="primary"
+						className="loginBtn"
+						htmlType="submit"
+					>
+						登陆
+					</Button>
 				</Row>
 				<Row className="ft-12">
-					<Col span={6}>忘记密码？</Col>
-					<Col span={12} onClick={() => type ? setType(0) : setType(1)}>{type ? "使用手机号码登陆" : "使用账户密码登陆"} {IconMap.arrRowRight}</Col>
+					<Col
+						span={6}
+						onClick={() => history.push('/users/forgetPassword')}
+					>
+						忘记密码？
+					</Col>
+					<Col
+						span={12}
+						onClick={() => (type ? setType(0) : setType(1))}
+					>
+						{type ? '使用手机号码登陆' : '使用账户密码登陆'}{' '}
+						{IconMap.arrRowRight}
+					</Col>
 				</Row>
 			</Form>
 		</div>
