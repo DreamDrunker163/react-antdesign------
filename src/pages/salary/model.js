b03f1080f8d5c478de0783ffb91fb8d7531186c9
DreamDrunker163 <3048710651@qq.com>
2023-09-23 14:45:06 +0800

    feat: salary数据编写

---
+++ src/pages/salary/model.js
@@ -0,0 +1,70 @@
+export default {
+	namespace: 'salary',
+	state: {
+		data: [
+			{
+				_id: '211302198305112819',
+				assessmentScore: 30,
+				currentLevel: {
+					_id: '46946',
+					levelName: 'T3-2'
+				},
+				date: '2017-8-10T08:50:25.121Z',
+				initLevel: {
+					_id: '123',
+					levelName: 'T9-1'
+				},
+				quarterly: 0,
+				result: 'C',
+				staffName: {
+					_id: '211302198305122819',
+					userName: '小明',
+					onboardingTime: '2017-8-10T08:50:25.121Z'
+				},
+				standardScore: 80
+			},
+			{
+				_id: '211302198305122819',
+				assessmentScore: 30,
+				currentLevel: {
+					_id: '46946',
+					levelName: 'T3-2'
+				},
+				date: '2017-8-10T08:50:25.121Z',
+				initLevel: {
+					_id: '123',
+					levelName: 'T9-1'
+				},
+				quarterly: 0,
+				result: 'C',
+				staffName: {
+					_id: '211302198305122819',
+					userName: '小明',
+					onboardingTime: '2017-8-10T08:50:25.121Z'
+				},
+				standardScore: 80
+			},
+			{
+				_id: '211302198305122819',
+				assessmentScore: 30,
+				currentLevel: {
+					_id: '46946',
+					levelName: 'T3-2'
+				},
+				date: '2017-8-10T08:50:25.121Z',
+				initLevel: {
+					_id: '123',
+					levelName: 'T9-1'
+				},
+				quarterly: 0,
+				result: 'C',
+				staffName: {
+					_id: '211302198305122819',
+					userName: '小明',
+					onboardingTime: '2017-8-10T08:50:25.121Z'
+				},
+				standardScore: 80
+			}
+		]
+	}
+}
