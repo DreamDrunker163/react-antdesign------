b92e80670b9349fd71e67bfe80639de59f0e198b
DreamDrunker163 <3048710651@qq.com>
2023-09-23 14:44:36 +0800

    feat: rewardAndPunishment数据编写

---
+++ src/pages/rewardAndPunishment/model.js
@@ -0,0 +1,31 @@
+export default {
+	namespace: 'rewardAndPunishment',
+	state: {
+		data: [
+			{
+				_id: '1',
+				fileList: [],
+				reason: '吃东西，警告处分',
+				recordDesc: '测试修改',
+				staffName: {
+					_id: '211302198305112819',
+					userName: '张飞'
+				},
+				type: 3,
+				date: '2021-03-02T16:00:00.000Z'
+			},
+			{
+				_id: '2',
+				fileList: [],
+				reason: '吃东西，警告处分',
+				recordDesc: '测试修改',
+				staffName: {
+					_id: '211302198305112819',
+					userName: '张飞'
+				},
+				type: 3,
+				date: '2021-03-02T16:00:00.000Z'
+			}
+		]
+	}
+}
